80b71b30880efdda2d2a135fa639aeb3a3a1d97b
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-10-16 14:38:56 +0100

    Gem generated

---
+++ spec/act_as_agent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActAsAgent do
+  it "has a version number" do
+    expect(ActAsAgent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
