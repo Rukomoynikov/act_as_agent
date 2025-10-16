380202528f1f7c2368af6d938b6f454afba273fb
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-10-16 18:42:53 +0100

    Setup VCR

---
+++ spec/support/vcr.rb
@@ -0,0 +1,11 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+
+  c.filter_sensitive_data("<X-Api-Key>") do |interaction|
+    interaction.request.headers["X-Api-Key"] = ""
+  end
+end
