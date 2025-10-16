a86ab524f42ea748155ba2aec30b8f13b46a9481
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-10-16 18:42:53 +0100

    Setup VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 
 require "act_as_agent"
 
+Dir.glob("spec/support/*.*").each { |file| require File.join(__dir__, "..", file) }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
