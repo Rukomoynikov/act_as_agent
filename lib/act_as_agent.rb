e566bc74ddfcfdfa4ed1bbd2fa3fb8e8a70e85ca
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-10-16 14:47:38 +0100

    Remove "version" module

--- lib/act_as_agent.rb
+++ lib/act_as_agent.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "act_as_agent/version"
 
 module ActAsAgent
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/act_as_agent_spec.rb
+++ spec/act_as_agent_spec.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe ActAsAgent do
-  it "has a version number" do
-    expect(ActAsAgent::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
