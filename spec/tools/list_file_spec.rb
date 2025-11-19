38819212b8260ed89dc0a385f2974398caea0c33
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-11-19 19:57:43 +0000

    Add tests for root_folder (both relative and absolute)

--- spec/tools/list_file_spec.rb
+++ spec/tools/list_file_spec.rb
@@ -5,6 +5,42 @@ require "tmpdir"
 require "tempfile"
 
 RSpec.describe ActAsAgent::Tools::ListFiles do
+  context "when root_folder is passed" do
+    context "when param is relative" do
+      let(:looked_path) { "./" }
+
+      it "returns error" do
+        tool = ActAsAgent::Tools::ListFiles.new(root_folder: looked_path)
+        expected_error = ActAsAgent::Errors::ToolIncorrectArgsError
+                         .new("Path is not absolute: #{looked_path}")
+
+        expect(tool.call).to eq(expected_error)
+      end
+    end
+
+    context "when param is absolute" do
+      let(:tmp_dir) { Dir.mktmpdir }
+
+      before do
+        Tempfile.create("file1.txt", tmp_dir)
+
+        nested_dir_path = File.join(tmp_dir, "nested_folder")
+        Dir.mkdir(nested_dir_path)
+        Tempfile.create("file_nested.txt", nested_dir_path)
+      end
+
+      let(:looked_path) { File.join(tmp_dir, "**/*.*") }
+
+      it "returns list of files" do
+        tool = ActAsAgent::Tools::ListFiles.new(root_folder: looked_path)
+
+        expect(
+          tool.call.length
+        ).to eq(2)
+      end
+    end
+  end
+
   context "when param path is passed" do
     context "when path is relative" do
       let(:looked_path) { "./" }
@@ -32,14 +68,12 @@ RSpec.describe ActAsAgent::Tools::ListFiles do
         Tempfile.create("file_nested.txt", nested_dir_path)
       end
 
-      describe "when glob parameter is passed" do
-        let(:looked_path) { File.join(tmp_dir, "**/*.*") }
+      let(:looked_path) { File.join(tmp_dir, "**/*.*") }
 
-        it "returns list of files" do
-          expect(
-            subject.call({ "path" => looked_path }).length
-          ).to eq(2)
-        end
+      it "returns list of files" do
+        expect(
+          subject.call({ "path" => looked_path }).length
+        ).to eq(2)
       end
     end
   end
